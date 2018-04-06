209cd187ecada912b0275a9530b85a2406570a40
Paul Smith <paulcsmith0218@gmail.com>
2018-04-06 17:41:44 -0400

    Test params

--- spec/send_grid_adapter_spec.cr
+++ spec/send_grid_adapter_spec.cr
@@ -3,6 +3,14 @@ require "./spec_helper"
 describe "SendGrid adapter" do
   describe "params" do
     it "sets personalizations" do
+      address = Carbon::Address.new("to@example.com")
+      address_2 = Carbon::Address.new("Jimmy", "to2@example.com")
+      params_for(to: [address, address_2])[:personalizations].should eq(
+        [
+          {:to => [{name: nil, email: "to@example.com"},
+                   {name: "Jimmy", email: "to2@example.com"}]},
+        ]
+      )
     end
 
     it "sets the subject" do
@@ -25,9 +33,9 @@ end
 private class FakeEmail < Carbon::Email
   def initialize(
     @from = Carbon::Address.new("from@example.com"),
-    @to = Carbon::Address.new("to@example.com"),
-    @cc = Carbon::Address.new("cc@example.com"),
-    @bcc = Carbon::Address.new("bcc@example.com"),
+    @to = [] of Carbon::Address,
+    @cc = [] of Carbon::Address,
+    @bcc = [] of Carbon::Address,
     @subject = "subject",
     @text_body = "text body",
     @html_body = "html body"
