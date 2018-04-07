f6a29eea7316cbfd0c59f3f7b2d55d62729f0836
Paul Smith <paulcsmith0218@gmail.com>
2018-04-07 12:15:04 -0400

    More robust testing

--- spec/expectations_spec.cr
+++ spec/expectations_spec.cr
@@ -6,10 +6,12 @@ describe Carbon::Expectations do
   it "can check for delivered emails" do
     adapter = Carbon::DevAdapter.new
     email = FakeEmail.new(subject: "Sent email")
+    other_email = FakeEmail.new(subject: "Other email")
     email.should_not be_delivered
 
     adapter.deliver_now(email)
 
     email.should be_delivered
+    other_email.should_not be_delivered
   end
 end
