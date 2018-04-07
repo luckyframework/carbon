7c3065f79653cfa300acb507d0fc5997b70b9a62
Paul Smith <paulcsmith0218@gmail.com>
2018-04-07 12:25:28 -0400

    Show nicer expectation message

--- src/carbon/expectations.cr
+++ src/carbon/expectations.cr
@@ -5,11 +5,30 @@ module Carbon::Expectations
     end
 
     def failure_message(email)
-      "Expected: #{email} to be delivered"
+      FailureMessage.new(email).build
     end
 
     def negative_failure_message(email)
       "Expected: #{email} not to be delivered"
+    end
+  end
+
+  class FailureMessage
+    private getter email
+
+    def initialize(@email : Carbon::Email)
+    end
+
+    def build
+      String.build do |message|
+        message << "Expected: #{email} to be delivered"
+        if Carbon::DevAdapter.delivered_emails.empty?
+          message << ", but no emails were delivered"
+        else
+          message << "\n\nTry this..."
+          message << "\n\n  ▸ See what emails were delivered with 'p Carbon::DevAdapter.delivered_emails'"
+        end
+      end
     end
   end
 
