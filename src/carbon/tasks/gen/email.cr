5f7c10ced30563ef9a3a47f178cb250fa452592c
Michael Wagner <mdwranger@gmail.com>
2023-05-25 19:32:54 -0400

    feat: swap teeplate for lucky_template

--- src/carbon/tasks/gen/email.cr
+++ src/carbon/tasks/gen/email.cr
@@ -1,15 +1,38 @@
 require "colorize"
-require "teeplate"
+require "lucky_template"
 require "wordsmith"
 require "file_utils"
+require "lucky_task"
+require "ecr"
 
-class Carbon::EmailTemplate < Teeplate::FileTree
-  directory "#{__DIR__}/templates"
+class Carbon::EmailTemplate
+  def initialize(@email_filename : String, @email_class_name : String)
+  end
 
-  def initialize(
-    @email_filename : String,
-    @email_class_name : String
-  )
+  def render(path : Path)
+    LuckyTemplate.write!(path, template_folder)
+  end
+
+  def template_folder
+    LuckyTemplate.create_folder do |top_dir|
+      top_dir.add_folder("src", "emails", "templates") do |templates_dir|
+
+        templates_dir.add_file("#{@email_filename}_email.cr") do |io|
+          ECR.embed("#{__DIR__}/templates/email.cr.ecr", io)
+        end
+
+        templates_dir.add_folder("#{@email_filename}_email") do |email_templates_dir|
+
+          email_templates_dir.add_file("html.ecr") do |io|
+            ECR.embed("#{__DIR__}/templates/html.ecr.ecr", io)
+          end
+
+          email_templates_dir.add_file("text.ecr") do |io|
+            ECR.embed("#{__DIR__}/templates/text.ecr.ecr", io)
+          end
+        end
+      end
+    end
   end
 end
 
@@ -31,10 +54,12 @@ class Gen::Email < LuckyTask::Task
   end
 
   def call
-    template = Carbon::EmailTemplate.new(filename, normalized_email_name)
-    template.render(output_path.to_s)
-
+    email_template.render(Path["."])
     display_success_messages
+  end
+
+  def email_template
+    Carbon::EmailTemplate.new(filename, normalized_email_name)
   end
 
   private def normalized_email_name : String
@@ -45,29 +70,17 @@ class Gen::Email < LuckyTask::Task
     Wordsmith::Inflector.underscore(normalized_email_name)
   end
 
-  private def output_path : Path
-    Path[".", "src", "emails"]
-  end
-
   private def display_success_messages
-    output.puts <<-MESSAGE
-    Generated email template
+    snapshot = LuckyTemplate.snapshot(email_template.template_folder)
+    paths = snapshot.reject { |_, v| v.folder? }.keys
 
-      #{green_arrow} #{email_class_template_path.colorize.bold}
-      #{green_arrow} #{email_class_template_file("html").colorize.bold}
-      #{green_arrow} #{email_class_template_file("text").colorize.bold}
-    MESSAGE
-  end
-
-  private def email_class_template_filename
-    "#{filename}_email.cr"
-  end
-
-  private def email_class_template_path
-    "src/emails/#{email_class_template_filename}"
-  end
-
-  private def email_class_template_file(file)
-    "src/emails/templates/#{filename}_email/#{file}.ecr"
+    output.puts "Generated email template"
+    output.puts
+    paths.each_with_index do |path, index|
+      output.print "  #{green_arrow} #{path.colorize.bold}"
+      unless index == paths.size - 1
+        output.print '\n'
+      end
+    end
   end
 end
