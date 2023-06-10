b3e27bdc18ca0f9551235e9f4e5d3de7baf1b532
Michael Wagner <mdwagner@users.noreply.github.com>
2023-06-09 21:01:24 -0400

    LuckyTemplate migration (#82)
    * feat: replace teeplate w/ lucky_template
    
    * chore: add windows to ci

--- spec/tasks/email_spec.cr
+++ spec/tasks/email_spec.cr
@@ -1,6 +1,7 @@
 require "../spec_helper"
 
 include CleanupHelper
+include LuckyTemplate::Spec
 
 describe Gen::Email do
   it "generates a new email" do
@@ -8,8 +9,10 @@ describe Gen::Email do
       generator = Gen::Email.new
       generator.output = IO::Memory.new
       generator.print_help_or_call ["PasswordReset"]
-
       generator.output.to_s.should contain("src/emails/templates/password_reset_email/html.ecr")
+
+      folder = generator.email_template.template_folder
+      folder.should be_valid_at(Path["."])
     end
   end
 
@@ -18,8 +21,10 @@ describe Gen::Email do
       generator = Gen::Email.new
       generator.output = IO::Memory.new
       generator.print_help_or_call ["WelcomeUserEmail"]
-
       generator.output.to_s.should contain("src/emails/templates/welcome_user_email/html.ecr")
+
+      folder = generator.email_template.template_folder
+      folder.should be_valid_at(Path["."])
     end
   end
 end
